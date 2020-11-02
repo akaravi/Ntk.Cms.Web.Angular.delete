4bb1b20728a4de722f591f2fcf3ec3872cef15d4
Alireza Karavi <home+akaravi@users.noreply.github.com>
2020-11-02 09:10:40 +0330

    loading

--- src/app/@cms/cmsComponents/_base/baseComponent.ts
+++ src/app/@cms/cmsComponents/_base/baseComponent.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectorRef } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { PublicHelper } from 'app/@cms/cmsCommon/helper/publicHelper';
-import { CmsToastrServiceService } from 'app/@cms/cmsService/_base/cmsToastrService.service';
 
 export class BaseComponent {
   constructor() {
